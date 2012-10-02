a61ad439a6eda2feeb0b2c7507461f53c2526f2e
Remy van Elst <rvanelst@digidentity.eu>
2012-10-02 22:23:29 +0200

    Added a header.php file

---
+++ header.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+#Copyright (c) 2012 Remy van Elst
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+*/
+
+include("functions.php");
+
+?>
+<!DOCTYPE html>
+<html><head>
+<title>Task / Todo list by Raymii.org</title>
+<meta charset="UTF-8">
+<meta name="description" content="" />
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<script type="text/javascript" src="js/prettify.js"></script>                                   
+<script type="text/javascript" src="js/kickstart.js"></script>                                  
+<link rel="stylesheet" type="text/css" href="css/kickstart.css" media="all" />                  
+<link rel="stylesheet" type="text/css" href="style.css" media="all" />                          
+</head><body><a id="top-of-page"></a><div id="wrap" class="clearfix">
+<div class="col_9">
