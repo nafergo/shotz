0bde8c9de3f8deaa31f8511421c7a3de4baa00c3
Remy van Elst <rvanelst@digidentity.eu>
2012-10-02 22:26:57 +0200

    Moved form and unfinished tasks to functions.php

--- index.php
+++ index.php
@@ -20,54 +20,15 @@
 #THE SOFTWARE.
 */
 
-$file = file_get_contents("task.json");
-$json_a = json_decode($file, true);
-$closed=0;
-$havetasks = 0;
-?>
-<!DOCTYPE html>
-<html><head>
-<title>Task / Todo list by Raymii.org</title>
-<meta charset="UTF-8">
-<meta name="description" content="" />
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<script type="text/javascript" src="js/prettify.js"></script>                                   
-<script type="text/javascript" src="js/kickstart.js"></script>                                  
-<link rel="stylesheet" type="text/css" href="css/kickstart.css" media="all" />                  
-<link rel="stylesheet" type="text/css" href="style.css" media="all" />                          
-</head><body><a id="top-of-page"></a><div id="wrap" class="clearfix">
-<div class="col_9">
+include("header.php");
 
-	<?php
 	echo "<h2>Tasks</h2>";
-	echo "<form name=\"edit\" action=\"action.php\" method=\"GET\">";
-	echo "<input name=\"content\" type=\"text\" placeholder=\"Add new task...\" ></input>";
-	echo "<input type=\"hidden\" name=\"action\" value=\"add\"></input>";
-	echo "&nbsp; &nbsp; ";
-	echo "<input type=\"submit\" name=\"submit\" value=\"Add Task\"></input>";
-	echo "</form>";
 
-	echo "<ul>";
-	if(is_array($json_a)) {		
-		foreach ($json_a as $item => $task) {
-			if ($task['status'] == "open") {	
-				$havetasks=1;
-				echo "<li>";
-				echo $task['task'];
-				echo " <a href=\"action.php?id=" .$item. "&action=done\"><span class=\"icon small darkgray\" data-icon=\"C\"></span></a>";
-				echo " [ <a href=\"action.php?id=" .$item. "&action=edit\"><span class=\"icon small darkgray\" data-icon=\"7\"></span></a>";
-				echo " <a href=\"action.php?id=" .$item. "&action=delete\"><span class=\"icon small darkgray\" data-icon=\"T\"></span></a> ] ";
-				echo "</li>";
-			}	
-		}
-		if($havetasks == 0) {
-			echo"No tasks. Add one first please.";	
-		}
-	} else { 
-		echo"No tasks. Add one first please.";	
-	}
-	echo "</ul>";
+	showinputform("action.php");
+
+	echo "<p></p>";
+
+	listtasks($json_a,"table");
 
 	echo"<h2>Finished Tasks</h2>";
 	echo "<ul>";
